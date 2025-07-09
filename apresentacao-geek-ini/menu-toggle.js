df009e3b98d49f5fa4d7312181c3754d17f0ca8c
Manoel Coelho <manoelccoelho@gmail.com>
2025-07-09 12:06:43 -0300

    correção carrocel

--- apresentacao-geek-ini/menu-toggle.js
+++ apresentacao-geek-ini/menu-toggle.js
@@ -1,18 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const btn = document.getElementById("hamburger-menu");
     const menu = document.getElementById("menu");
-
-    btn.addEventListener("click", (e) => {
-        e.stopPropagation();
-        menu.classList.toggle("menu-open");
-        btn.classList.toggle("is-active");
-    });
 
     // Fechar menu ao clicar fora
     document.addEventListener("click", (e) => {
-        if (!menu.contains(e.target) && !btn.contains(e.target)) {
+        if (!menu.contains(e.target)) {
             menu.classList.remove("menu-open");
-            btn.classList.remove("is-active");
         }
     });
 });
